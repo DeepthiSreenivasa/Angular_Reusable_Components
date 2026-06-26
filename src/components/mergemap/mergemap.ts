96b06f7a863500004bbee6a07e42b87b5c061445
sreenid <deepthi.sreenivasa@mercedes-benz.com>
2026-06-26 13:46:47 +0530

    added promise apis

--- src/components/mergemap/mergemap.ts
+++ src/components/mergemap/mergemap.ts
@@ -23,4 +23,10 @@ export class Mergemap { //We dont want the sequence jus  that it should all star
 
   from([this.getUsers,this.getOrders,this.getNotification]).pipe(mergemap(api=>api).subscribe(data=>console.log(data)))
 
+
+  //all - waits fro all even if one fails everything else faisl
+  //race - win / lose fisr one returns
+  //allsettles - bith failed and success ones return
+  //any - give first success
+
 }
