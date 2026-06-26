565fbe37954dd0f98e6263148b2df11817bd6a8a
sreenid <deepthi.sreenivasa@mercedes-benz.com>
2026-06-26 17:57:08 +0530

    added few more interview concepts

---
+++ src/components/router-component/router-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-router-component',
+  imports: [],
+  templateUrl: './router-component.html',
+  styleUrl: './router-component.css',
+})
+export class RouterComponent {}
