099d39ba438537c094f12ec3fb94d1d7d9b38feb
sreenid <deepthi.sreenivasa@mercedes-benz.com>
2026-06-26 12:42:44 +0530

    creating reactive form

---
+++ src/components/reactive-forms/reactive-forms.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './reactive-forms.html',
+  styleUrl: './reactive-forms.css',
+})
+export class ReactiveForms {
+  userForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+  });
+
+  getFormData() {
+    console.log(this.userForm);
+  }
+}
